5dc194e2497cf40d6897891f5bee0ec8b111ee29
seoseunghyeon <gustjtmd@gmail.com>
2022-06-01 16:59:15 +0900

    JpaWebApplication

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+
+@Getter @Setter
+public class Hello {
+
+    private String data;
+}
